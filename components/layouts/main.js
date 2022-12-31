9dfd1fcadc1b989191f628a5e3013a14cfb81c0f
kitakenkani <kitaken9973@gmail.com>
2022-12-31 13:56:51 +0900

    fix seo

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -12,7 +12,15 @@ const Main = ({ children, router }) => {
                 <meta name="description" content="Tsumugi homepage" />
                 <meta name="author" content="Kentaro Kitamura" />
                 <meta name="author" content="マンドリンオーケストラ紬" />
-                <link rel="apple-touch-icon" href="logo.svg" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+                <link rel="icon" type="image/png" href="/favicons/android-chrome-192x192.png" sizes="192x192"/>
+                <link rel="icon" type="image/png" href="/favicons/android-chrome-512x512.png" sizes="512x512"/>
+                <link rel="manifest" href="/favicons/webmanifest.json" />
+                <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+                <link rel="shortcut icon" href="/favicons/favicon.ico" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta name="twitter:title" content="マンドリンオーケストラ紬 ホームページ" />
                 <meta name="twitter:card" ntent="summary_large_image" />
                 <meta name="twitter:site" content="@TumugiMO" />
