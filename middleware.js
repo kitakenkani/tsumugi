276aaa26978e79d20b7c6ae22a0f1d5364fe05b0
kitakenkani <kitaken9973@gmail.com>
2024-04-13 22:40:00 +0900

    cookie test

--- middleware.js
+++ middleware.js
@@ -1,6 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request) {
+    let cookie = request.cookies.get(nextjs);
+    const allCookies = request.cookies.getAll();
+    
+    const response = NextResponse.next()
+    response.cookies.set('vercel', 'fast')
+    response.cookie.set({
+        name: 'vercel',
+        value: 'fast',
+        path: '/',
+    })
     if (request.nextUrl.pathname === '/about') {
         return NextResponse.redirect(new URL('/redirected', request.url));
     }
